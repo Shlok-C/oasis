6b0ef97310441ddef700548bb292d34d8bcc5948
Shlok Chatterjee <shlok.chatterjee007@gmail.com>
2026-02-24 00:09:40 -0600

    cudaq in cpp

---
+++ dla_compute/src/compute_dla.cpp
@@ -0,0 +1,35 @@
+#include <cudaq.h>
+#include <cudaq/algorithm.h>
+#include <cudaq/spin_op.h>
+
+#include <map>
+#include <string>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+__qpu__ void bell_kernel(int n_qubits) {
+    cudaq::qvector q(n_qubits);
+    h(q[0]);
+    for (int i = 0; i < n_qubits - 1; i++)
+        x<cudaq::ctrl>(q[i], q[i + 1]);
+    mz(q);
+}
+
+std::map<std::string, int> sample_bell(int n_qubits, int shots) {
+    auto result = cudaq::sample(shots, bell_kernel, n_qubits);
+    std::map<std::string, int> counts;
+    for (auto &[bitstring, count] : result)
+        counts[bitstring] = static_cast<int>(count);
+    return counts;
+}
+
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(compute_dla, m) {
+    m.doc() = "CUDA-Q accelerated DLA computation";
+    m.def("sample_bell", &sample_bell,
+          py::arg("n_qubits"), py::arg("shots") = 1000,
+          "Sample a Bell state circuit and return bitstring -> count dict");
+}
